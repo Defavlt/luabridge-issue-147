a99d393ba6600dc4d26223c5405c9607dcf1c066
Marcus Hansson <marcus.hansson@email.com>
2017-11-03 01:37:49 +0100

    Add simple POD as test data

---
+++ src/table.h
@@ -0,0 +1,35 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <string>
+
+struct Table0 {
+    Table0()
+        : m_str("Hello, world!")
+        , m_int(0)
+        , m_bool(false)
+    {}
+
+    Table0(Table0 &&) = delete;
+    Table0(Table0 &) = delete;
+
+    std::string m_str;
+    int m_int;
+    bool m_bool;
+};
+
+struct Table1 {
+    Table1() : nested()
+    {}
+
+    Table1(Table1 &&) = delete;
+    Table1(Table1 &) = delete;
+
+    Table0 nested;
+
+    const Table0 &get() const
+    { return nested;
+    }
+};
+
+#endif//TABLE_H
